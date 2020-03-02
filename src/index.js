1e4ec8fc32749eb3337b5154f6727314d5904760
Jim Skretas-Kazakis <jimskretas@gmail.com>
2020-03-02 21:35:58 +0200

    Support multiple lists

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import initialData from "./initial-data";
 import { DragDropContext } from "react-beautiful-dnd";
 
 export default function App() {
-  //state = initialData;
   const [state, setState] = useState(initialData);
 
   function onDragEnd(result) {
@@ -22,21 +21,54 @@ export default function App() {
       return;
     }
 
-    const column = state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const startColumn = state.columns[source.droppableId];
+    const destColumn = state.columns[destination.droppableId];
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds
+    //Moving cards in the same list
+    if (startColumn === destColumn) {
+      const newTaskIds = Array.from(startColumn.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...startColumn,
+        taskIds: newTaskIds
+      };
+
+      const newState = {
+        ...state,
+        columns: {
+          ...state.columns,
+          [newColumn.id]: newColumn
+        }
+      };
+
+      setState(newState);
+      return;
+    }
+
+    //Moving from one list to another
+    const startTaskIds = Array.from(startColumn.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...startColumn,
+      taskIds: startTaskIds
+    };
+
+    const finishTaskIds = Array.from(destColumn.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+
+    const newFinish = {
+      ...destColumn,
+      taskIds: finishTaskIds
     };
 
     const newState = {
       ...state,
       columns: {
         ...state.columns,
-        [newColumn.id]: newColumn
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish
       }
     };
 
--- src/initial-data.js
+++ src/initial-data.js
@@ -9,10 +9,20 @@ const initialData = {
     "column-1": {
       id: "column-1",
       title: "To do",
-      taskIds: ["task-1", "task-2", "task-3", "task-4"]
+      taskIds: ["task-1", "task-2"]
+    },
+    "column-2": {
+      id: "column-2",
+      title: "Doing",
+      taskIds: ["task-3"]
+    },
+    "column-3": {
+      id: "column-3",
+      title: "Done",
+      taskIds: ["task-4"]
     }
   },
-  columnOrder: ["column-1"]
+  columnOrder: ["column-1", "column-2", "column-3"]
 };
 
 export default initialData;
