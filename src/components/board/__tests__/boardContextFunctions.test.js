b8a216d9930844b292aebab9c992cced966c670e
Jim Skretas <jimskretas@gmail.com>
2020-03-19 20:27:10 +0200

    Finish tests for boardContextFunctions

--- src/components/board/__tests__/boardContextFunctions.test.js
+++ src/components/board/__tests__/boardContextFunctions.test.js
@@ -160,8 +160,104 @@ it("Should not change state, content hasn't changed", function() {
   expect(updateCardFunction(state, content, cardId)).toEqual(state);
 });
 
-// it("Should move card in the same list", function() {
-//   let result = {};
-//   let targetState = {};
-//   expect(updateCardFunction(state, result)).toEqual(state);
-// });
+it("Should move card in the same list", function() {
+  let targetState = {
+    cards: {
+      card1: { id: "card1", content: "Take out the garbage" },
+      card2: { id: "card2", content: "Watch para 5" },
+      card3: { id: "card3", content: "Charge phone" },
+      card4: { id: "card4", content: "Cook dinner" }
+    },
+    columns: {
+      backlog: {
+        id: "backlog",
+        title: "Backlog",
+        cardIds: []
+      },
+      todo: {
+        id: "todo",
+        title: "To do",
+        cardIds: ["card2", "card1"]
+      },
+      doing: {
+        id: "doing",
+        title: "Doing",
+        cardIds: ["card3"]
+      },
+      done: {
+        id: "done",
+        title: "Done",
+        cardIds: ["card4"]
+      }
+    },
+    columnOrder: ["backlog", "todo", "doing", "done"],
+    cardNumber: 4
+  };
+  let result = {
+    draggableId: "card2",
+    type: "DEFAULT",
+    source: {
+      index: 1,
+      droppableId: "todo"
+    },
+    reason: "DROP",
+    mode: "FLUID",
+    destination: {
+      droppableId: "todo",
+      index: 0
+    },
+    combine: null
+  };
+  expect(moveCardFunction(state, result)).toEqual(targetState);
+});
+
+it("Should move card in different list", function() {
+  let targetState = {
+    cards: {
+      card1: { id: "card1", content: "Take out the garbage" },
+      card2: { id: "card2", content: "Watch para 5" },
+      card3: { id: "card3", content: "Charge phone" },
+      card4: { id: "card4", content: "Cook dinner" }
+    },
+    columns: {
+      backlog: {
+        id: "backlog",
+        title: "Backlog",
+        cardIds: ["card2"]
+      },
+      todo: {
+        id: "todo",
+        title: "To do",
+        cardIds: ["card1"]
+      },
+      doing: {
+        id: "doing",
+        title: "Doing",
+        cardIds: ["card3"]
+      },
+      done: {
+        id: "done",
+        title: "Done",
+        cardIds: ["card4"]
+      }
+    },
+    columnOrder: ["backlog", "todo", "doing", "done"],
+    cardNumber: 4
+  };
+  let result = {
+    draggableId: "card2",
+    type: "DEFAULT",
+    source: {
+      index: 1,
+      droppableId: "todo"
+    },
+    reason: "DROP",
+    mode: "FLUID",
+    destination: {
+      droppableId: "backlog",
+      index: 0
+    },
+    combine: null
+  };
+  expect(moveCardFunction(state, result)).toEqual(targetState);
+});
