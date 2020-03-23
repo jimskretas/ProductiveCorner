de097a30462c78e107ee8d3b89fd5fe2b629cfa3
Jim Skretas <jimskretas@gmail.com>
2020-03-24 00:13:59 +0200

    Add test for add card button

--- src/components/board/__tests__/boardTests.test.js
+++ src/components/board/__tests__/boardTests.test.js
@@ -1,8 +1,8 @@
 import React from "react";
-//import { createMount } from "@material-ui/core/test-utils";
+import { createMount } from "@material-ui/core/test-utils";
 import Board from "../Board";
 import { BoardProvider } from "../BoardContext";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 function renderBoard() {
   return render(
@@ -13,19 +13,28 @@ function renderBoard() {
 }
 
 describe("Board Integration Tests", () => {
-  //let mount;
+  let mount;
 
   beforeEach(() => {
-    //mount = createMount();
+    mount = createMount();
   });
 
   afterEach(() => {
-    //mount.cleanUp();
+    mount.cleanUp();
     cleanup();
   });
 
-  it("Should render cards correctly", () => {
+  it("Should render cards", () => {
     const { getAllByDisplayValue } = renderBoard();
     expect(getAllByDisplayValue("Charge phone").length).toBe(1);
+  });
+
+  it("Should add a card", () => {
+    const { getAllByDisplayValue, getAllByRole } = renderBoard();
+
+    const addCardButton = getAllByRole("button", { name: "Add Card" });
+    fireEvent.click(addCardButton[0]);
+
+    expect(getAllByDisplayValue("").length).toBe(1);
   });
 });
