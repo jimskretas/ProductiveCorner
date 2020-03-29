3c180e1b5cbbb6e3ddd0f941bcb379dbb5f96526
Jim Skretas <jimskretas@gmail.com>
2020-03-29 13:53:29 +0300

    Update NavBar

--- src/components/board/NavBar.js
+++ src/components/board/NavBar.js
@@ -4,6 +4,10 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import InfoIcon from "@material-ui/icons/Info";
+import Tooltip from "@material-ui/core/Tooltip";
 import { Link as RouterLink } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
@@ -22,9 +26,16 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Productive Corner
           </Typography>
-          <Button color="inherit" component={RouterLink} to="/login">
-            Logout
-          </Button>
+          <Tooltip title="Productivity Tips">
+            <IconButton color="inherit" component={RouterLink} to="/login">
+              <InfoIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Logout">
+            <IconButton color="inherit" component={RouterLink} to="/login">
+              <ExitToAppIcon />
+            </IconButton>
+          </Tooltip>
         </Toolbar>
       </AppBar>
     </div>
